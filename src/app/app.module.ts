b05532bc196928d4cccbb80621c750fc726ed3bd
rronkurtishi <rron.kurtishi@gmail.com>
2020-10-13 23:44:12 +0200

    added contact form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { FooterComponent } from './components/footer/footer.component';
     ProfileComponent,
     ProjectsComponent,
     FooterComponent,
+    ContactFormComponent,
   ],
   imports: [
     BrowserModule,
